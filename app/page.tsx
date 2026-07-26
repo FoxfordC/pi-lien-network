c780afb484ae9b468811ae02c8f8cfc53f18eb94
FoxfordC <conor@foxfordconsulting.com>
2026-07-26 00:31:12 -0600

    Force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import dynamic from 'next/dynamic';
 import { useState } from 'react';
 import Navbar from './components/Navbar';
+export const dynamic = 'force-dynamic';
 
 const Map = dynamic(() => import('./ProviderMap'), { ssr: false });
 
